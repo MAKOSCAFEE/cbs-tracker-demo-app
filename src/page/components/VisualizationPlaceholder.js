48a36caa8be2ae371666be07b76aa7a465a2ea3c
Barnabas Makonda <barnabasmakonda@gmail.com>
2018-03-20 13:28:09 +0300

    Add SyncScroll for tables

---
+++ src/page/components/VisualizationPlaceholder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './VisualizationPlaceholder.css';
+
+export default class VisualizationPlaceholder extends Component {
+    render() {
+        return (
+            <div className="placeholder">
+                <h3>No Data To visualize</h3>
+            </div>
+        );
+    }
+}
--- src/page/index.js
+++ src/page/index.js
@@ -1,47 +1,17 @@
 import React, { Component } from 'react';
 import './page.css';
-// import { Column, Table } from 'react-virtualized';
 import { connect } from 'react-redux';
-
-const ValueTable = ({ data, isLoading }) => {
-    if (!data) {
-        return 'No values here';
-    }
-
-    if (isLoading) {
-        return 'Loading...';
-    }
-
-    const { headers, metaData, rows } = data;
-
-    return (
-        <section>
-            <table>
-                <thead>
-                    <tr>{headers.map((header, index) => <th key={index}>{header.column}</th>)}</tr>
-                </thead>
-                <tbody>
-                    {rows.map((row, index) => (
-                        <tr key={index}>
-                            {row.map((value, index) => (
-                                <td key={index}>{(metaData.items[value] || {}).name || value}</td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </section>
-    );
-};
+import LineList from './components/LineList';
+import VisualizationPlaceholder from './components/VisualizationPlaceholder';
 
 class TrackerReport extends Component {
     render() {
-        const { data, isLoading } = this.props;
-        // const width = typeof window === 'object' ? 0.65 * window.innerWidth : 450;
-        // const height = typeof window === 'object' ? 0.65 * window.innerHeight : 600;
+        const { data } = this.props;
+
         return (
             <div className="page">
-                <ValueTable data={data} isLoading={isLoading} />;
+                {!data && <VisualizationPlaceholder />}
+                {data && <LineList data={data} />}
             </div>
         );
     }
